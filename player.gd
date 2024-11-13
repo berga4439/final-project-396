f2fa480ae76299e394401a7b8eada61225298c96
berga4439 <123572908+berga4439@users.noreply.github.com>
2024-11-12 22:44:22 -0600

    Lerp player speed

--- player.gd
+++ player.gd
@@ -2,11 +2,13 @@ extends CharacterBody3D
 
 @onready var head: Node3D = $Head
 
-const SPEED = 3.0
+const SPEED = 3.5
 const JUMP_VELOCITY = 0.0
 
-
+@export var LERP_SPEED = 12.5
 @export var MOUSE_SENSITIVITY = 0.12
+
+var direction = Vector3.ZERO
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -31,7 +33,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta*LERP_SPEED)
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
