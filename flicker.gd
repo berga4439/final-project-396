b2ec4bf4bda03cf2124b6d207598631cc2d55dc7
berga4439 <123572908+berga4439@users.noreply.github.com>
2024-11-13 14:04:12 -0600

    Light flicker and shadows

---
+++ flicker.gd
@@ -0,0 +1,22 @@
+extends OmniLight3D
+
+@export var MAX_LIGHT = 0.4
+@export var MIN_LIGHT = 0.05
+@export var noise: NoiseTexture2D
+
+var current_light = (MIN_LIGHT + MAX_LIGHT) / 2.0
+var time_passed = 0.0
+var offset = 0.0
+
+func _ready() -> void:
+	noise.noise.seed = randi()
+	offset = randf() * 100.0
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	time_passed += delta
+	var sampled_noise = noise.noise.get_noise_1d(offset + time_passed * 6)
+	sampled_noise = abs(sampled_noise)
+	
+	current_light = MIN_LIGHT + (sampled_noise * (MAX_LIGHT-MIN_LIGHT))
+	light_energy = current_light
